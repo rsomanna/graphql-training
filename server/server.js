f207392a618c45fd64425737657ab5ee4e022221
somanna.ramaiah <somanna.ramaiah@.ibm.com>
2022-05-21 19:31:54 +0530

    apollo server added

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const PORT = 3500;
 const app = express();
 
 app.use("/graphql",(req, res) => {
-    res.send(" Welcome to GraphQL app new file");
+    res.send(" Welcome to GraphQL app nested chages.");
 } );
 
 app.listen(PORT ,() => {
